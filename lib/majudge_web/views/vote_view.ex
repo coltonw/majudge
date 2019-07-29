187ca213eb96cb4da88698409afd1638fcbfaf38
Will Colton <coltonw@gmail.com>
2019-07-28 21:53:19 -0400

    Continue improving create vote UI.

--- lib/majudge_web/views/vote_view.ex
+++ lib/majudge_web/views/vote_view.ex
@@ -4,4 +4,15 @@ defmodule MajudgeWeb.VoteView do
   def ballot_options(ballots) do
     ballots |> Enum.map(fn b -> {b.name, b.id} end)
   end
+
+  def ratings() do
+    [
+      excellent: "Extremely Interested",
+      verygood: "Very Interested",
+      good: "Interested",
+      average: "Ambivalent",
+      fair: "Uninterested",
+      poor: "Extremely Uninterested"
+    ]
+  end
 end
