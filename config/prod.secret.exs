5ca2c47b52d767aa3e9a1420f172ff13ad5dd9de
Will Colton <coltonw@gmail.com>
2019-08-13 07:28:16 -0400

    Properly fix the port.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,7 +23,7 @@ secret_key_base =
     """
 
 config :majudge, MajudgeWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "80")],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
